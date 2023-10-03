1f488b33f53ddd8798b2d7ab9c213e243fa57d26
Venomsnaker <146599764+Venomsnaker@users.noreply.github.com>
2023-10-03 20:38:49 +0700

    Learn SQL Basic
    FROM, SELECT, WHERE, LIKE, and REGEXP

---
+++ term-3rd/exercises-slides/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    int hex = 0x20;
+    printf("hex = %d", hex);
+}
---
+++ term-3rd/sql/general.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM customers
+-- WHERE first_name = "Babara"
+ORDER BY first_name 
