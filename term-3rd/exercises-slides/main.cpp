2c2bd84deef4cf765e5ef1ead377302f8d8d6fa0
Venomsnaker <146599764+Venomsnaker@users.noreply.github.com>
2023-10-05 21:30:02 +0700

    Basic Hashing

--- term-3rd/exercises-slides/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int hex = 0x20;
-    printf("hex = %d", hex);
-}
---
+++ term-3rd/trees/hashing.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+
+using namespace std;
+
+
+const int P = 1e6 + 3;
+
+struct HashTable {
+    vector< pair<int,int> > h[P];
+
+public:
+    void insert(int key, int value) {
+        int hkey = getHash(key);
+        for (auto p : h[hkey]) {
+            if (p.first == key) {
+                // key da ton tai trong Hash table, ta bo qua
+                return;
+            }
+        }
+        // Them (key, value) vao hash table
+        h[hkey].emplace_back(key, value);
+    }
+
+    int find(int key) {
+        int hkey = getHash(key);
+        for(auto p : h[hkey]) {
+            if (p.first == key) {
+                // ton tai key trong Hash table, return value
+                return p.second;
+            }
+        }
+        // Khong tim thay
+        return 0;
+    }
+
+private:
+    int getHash(int key) {
+        // Cho 1 key, tra lai Hash value la key % P
+        return key % P;
+    }
+};
+
