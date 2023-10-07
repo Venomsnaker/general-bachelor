78fc5a375e8bdc489d347883eb1e8b6972f24ae5
Venomsnaker <146599764+Venomsnaker@users.noreply.github.com>
2023-10-07 07:27:35 +0700

    Pointers

---
+++ term-3rd/trees/pointers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Example() {
+    string food = "Pizza";
+    string* ptr = &food;
+
+    cout << food << "-" << ptr << "-" << *ptr << endl;
+    
+    food = "Hotdog";
+    cout << food << "-" << ptr << "-" << *ptr << endl;
+}
+
+int main(){
+    Example();
+    return 0;
+}
