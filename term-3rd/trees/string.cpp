f2571d27cdfc8d00c39f01fc3f35d26407cac405
Venomsnaker <146599764+Venomsnaker@users.noreply.github.com>
2023-10-18 14:35:00 +0700

    String Operations

--- term-3rd/trees/string.cpp
+++ term-3rd/trees/string.cpp
@@ -11,18 +11,47 @@ void Example(char *s) {
     printf("Love ~ love, %d \n", stricmp("Love", "love"));
 }
 
-void CopyStringN() {
-    char s[] = "Tony Blurt";
-    char s2[100], *s3;
+void CopyString(char *s) {
+    char s2[100];
+    char *s3;
     strcpy(s2, s);
-    printf("%s\n", s2);
-    strncpy(s2 + 2, "12345", 3);
-    printf("%s\n", s2);
+    printf("%s \n", s2);
+    strncpy(s2 + 2, "12345", 4);
+    printf("%s \n", s2);
     s3 = strdup(s + 5);
-    printf("%s\n", s3);
+    printf("%s \n", s3);
     free(s3);
 }
 
+void SeparateString(char *s) {
+    char *p = strtok(s, " ");
+    while (p != NULL) {
+        printf("%s \n", p);
+        p = strtok(NULL, " ");
+    }
+}
+
+void SearchSubstring(char *s) {
+    char *p;
+    p = strchr(s, 's');
+    printf("%s \n");
+    p = strstr(s, "so");
+    printf("%s \n");
+}
+
+void InsertString(char *s, char *sub) {
+    int n = strlen(sub);
+    memmove(s + n, s, strlen(s));
+    strncpy(s, sub, n);
+    printf("%s \n", s);
+}
+
+void DeleteString(char *s, int n) {
+    memmove(s, s + n, strlen(s + n) + 1);
+}
+
 int main() {
-    char *s = strdup("We're so f");
+    char *s = strdup("Wes're so f");
+    char *i = strdup("Rip ");
+    InsertString(s, i);
 }
