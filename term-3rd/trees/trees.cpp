43d1c5ecb87e094c85d9a083c93dc351befc480f
Venomsnaker <146599764+Venomsnaker@users.noreply.github.com>
2023-10-22 19:04:59 +0700

    Binary Tree

--- term-3rd/trees/trees.cpp
+++ term-3rd/trees/trees.cpp
@@ -1,148 +1,93 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNode
-{
-    int data;
-    TreeNode *left;
-    TreeNode *right;
+struct Node {
+    int val;
+    Node *left;
+    Node *right;
 
-    TreeNode(int data)
-    {
-        this->data = data;
+    Node(int val) {
+        this->val = val;
         left = right = NULL;
     }
 };
 
-class BinaryTree
-{
-public:
-    // Recursive Tree Traversals
-    // Time Complexity: O(n)
-    // Space Complexity: O(1)
-    vector<int> inorderTraversal(TreeNode *root)
-    {
-        vector<int> nodes;
-        helper(root, nodes);
-        return nodes;
-    }
+class BinaryTree {
+    Node *root;
 
-    vector<int> preorderTraversal(TreeNode *root)
-    {
-        vector<int> nodes;
-        helper(root, nodes);
-        return nodes;
-    }
+    public:
+        Node* CreateNode(int val) {
+            Node* newNode = new Node(val);
+            if (!newNode) {
+                cout << "Memory Error \n";
+                return NULL;
+            }
+            return newNode;
+        }
 
-    vector<int> postorderTraversal(TreeNode *root)
-    {
-        vector<int> nodes;
-        helper(root, nodes);
-        return nodes;
-    }
+        void InsertNode(int val) {
+            if (root == NULL) {
+                root = CreateNode(val);
+                return;
+            }
 
-    vector<vector<int>> levelOrder(TreeNode *root)
-    {
-        // Time Complexity: O(n)
-        // Space Complexity: O(n)
-        vector<vector<int>> res;
-        queue<TreeNode *> temp;
-        temp.push(root);
+            queue<Node*> q;
+            q.push(root);
 
-        while (!temp.empty())
-        {
-            int curLen = temp.size();
-            vector<int> curLevel;
+            while (!q.empty()) {
+                Node* temp = q.front();
+                q.pop();
 
-            for (int i = 0; i < curLen; i++)
-            {
-                TreeNode *node = temp.front();
-                temp.pop();
-                if (node != NULL)
-                {
-                    curLevel.push_back(node->data);
-                    temp.push(node->left);
-                    temp.push(node->right);
+                if(temp->left != NULL) q.push(temp->left);
+                else {
+                    temp->left = CreateNode(val);
+                    return;
+                }
+
+                if(temp->right != NULL) q.push(temp->right);
+                else {
+                    temp->right = CreateNode(val);
+                    return;
                 }
             }
-
-            if (curLevel.size())
-            {
-                res.push_back(curLevel);
-            }
         }
-        return res;
-    }
 
-    int maxDepth(TreeNode *node) {
-        if (node == NULL) return 0;
-        else {
-            int lDepth = maxDepth(node->left);
-            int rDepth = maxDepth(node->right);
-
-            if (lDepth > rDepth) return (lDepth + 1);
-            else return (rDepth + 1);
+        void PrintTree() {
+            vector<int> nodes;
+            InorderTraversal(root, nodes);
+            for (int i = 0; i < nodes.size(); i++) cout << nodes[i] << endl;
         }
-    }
 
-    void printGivenLevel(node* root, int level); 
-int height(node* node); 
+    private:
+        // All basic traversal types
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        void InorderTraversal(Node *root, vector<int> &nodes) {
+            if (!root) return;
+            InorderTraversal(root->left, nodes);
+            nodes.push_back(root->val);
+            InorderTraversal(root->right, nodes);
+        }
 
-    void reverseLevelOrder(*node root) 
-{ 
-    int h = height(root); 
-    int i; 
-    for (i=h; i>=1; i--) //THE ONLY LINE DIFFERENT FROM NORMAL LEVEL ORDER 
-        printGivenLevel(root, i); 
-} 
- 
-/* Print nodes at a given level */
-void printGivenLevel(*node root, int level) 
-{ 
-    if (root == NULL) 
-        return; 
-    if (level == 1) 
-        cout << root->data << " "; 
-    else if (level > 1) 
-    { 
-        printGivenLevel(root->left, level - 1); 
-        printGivenLevel(root->right, level - 1); 
-    } 
-} 
+        void PreorderTraversal(Node *root, vector<int> &nodes) {
+            if (!root) return;
+            PreorderTraversal(root->left, nodes);
+            nodes.push_back(root->val);
+            PreorderTraversal(root->right, nodes);
+        }
 
-private:
-    void helper(TreeNode *root, vector<int> &nodes)
-    {
-        if (!root)
-            return;
-
-        helper(root->left, nodes);
-        nodes.push_back(root->data);
-        helper(root->right, nodes);
-    }
+        void PostorderTraversal(Node *root, vector<int> &nodes) {
+            if (!root) return;
+            PostorderTraversal(root->left, nodes);
+            nodes.push_back(root->val);
+            PostorderTraversal(root->right, nodes);
+        }
 };
 
-void TraverseInorder(struct TreeNode *root)
-{
-    stack<TreeNode *> s;
-    TreeNode *curr = root;
-
-    while (curr != NULL || s.empty() == false)
-    {
-        while (curr != NULL)
-        {
-            s.push(curr);
-            curr = curr->left;
-        }
-
-        curr = s.top();
-        s.pop();
-        cout << curr->data << " ";
-        curr = curr->right;
-    }
-}
-
-int main()
-{
-    return 0;
+int main() {
+    BinaryTree tree;
+    tree.InsertNode(1);
+    tree.InsertNode(2);
+    tree.InsertNode(3);
+    tree.PrintTree();
 }
