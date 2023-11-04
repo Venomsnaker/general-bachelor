5fd7cbdb9c3c0a9792d52169c604250c25ea3272
Venomsnaker <146599764+Venomsnaker@users.noreply.github.com>
2023-11-04 18:43:29 +0700

    Linked List (1)

--- term-3rd/algorithms/linked-list.cpp
+++ term-3rd/algorithms/linked-list.cpp
@@ -1,22 +1,63 @@
 #include <iostream>
 using namespace std;
 
-struct node {
+struct Node {
     int data;
-    node *next;
+    Node* next;
+    
+    Node() {
+        data = 0;
+        next = NULL;
+    }
 
-    node(int new_data) {
-        data = new_data;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
     }
 };
 
-void push(node *head, int data) {
-    node *new_node = new node(data);
-    new_node->next = head;
-    head = new_node;
-}
+class LinkedList {
+    Node *head;
+
+public:
+    LinkedList() {head = NULL;}
+
+    void insertNode(int data) {
+        Node* newNode = new Node(data);
+
+        if (head == NULL) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void deleteNode(int pos) {
+        
+    }
+
+    void printList() {
+        Node* temp = head;
+
+        if (head == NULL) {
+            cout << "List empty" << endl;
+            return;
+        }
+
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+
+};
 
 int main() {
-    cout << "Hello Hell!" << endl;
-    return 0;
+
 }
